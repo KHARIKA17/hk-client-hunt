85a5b6de1d1920a99d23de849bca28c7519b4f4d
KHARIKA17 <60010885+KHARIKA17@users.noreply.github.com>
2021-01-27 19:58:47 -0600

    due

---
+++ init-locations.js
@@ -0,0 +1,22 @@
+export default [
+    {
+      Name: 'Mesa',
+      North: 33.42743408994961,
+      West: -111.88336427583012,
+      South: 33.42743408994432,
+      East: -111.88336427583023,
+      Notes: 'circle',
+      Attachments: '',
+      'Quest-Location': 'Mesa',
+    },
+    {
+      Name: 'Faustiana',
+      North: 40.343579,
+      West: -94.89084,
+      South: 40.343268,
+      East: -94.8884,
+      Notes: 'heading south',
+      Attachments: '',
+      'Quest-Location': 'Faustiana-Faustiana',
+    },
+];
--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,43 @@
-window.addEventListener('load', (main) => {
-    console.log('page loaded');
-});
-locationElement.addEventListener('click', (locationHandler) => {
-    console.log('click registered')
-});
-locationElement.addEventListener('touch', (locationHandler) => {
-    console.log('touch recognized')
-});
+import getLocation from './location.js';
+
+
+
+
+async function locationHandler() {
+    const locText = await getLocation();
+    document.getElementById('locationAnswer').innerHTML = locText;
+}
+
+
+function clearErrorText() {
+    document.getElementById('error-message').innerHTML = '';
+}
+
+
+
+// main method
+function main() {
+    console.log('Starting main method...');
+
+    // get references to html elements
+    const locationElement = document.getElementById('location');
+    const errorElement = document.getElementById('error-message');
+
+    // init error to empty string
+    errorElement.innerHTML = '';
+
+    locationElement.addEventListener('click', locationHandler)
+    locationElement.addEventListener('touch', locationHandler);
+    
+}
+
+
+
+
+
+
+
+
+//starting point where it begins
+window.addEventListener('load', main);
+
